908fe592014f24f8d080012b7fa992f4ed2131a3
jpb <john.peter.berntsson@smartrefill.se>
2018-05-30 22:26:40 +0200

    Adding recyclerView

--- app/src/main/java/com/jpberntsson/materialdesigns/MainActivity.kt
+++ app/src/main/java/com/jpberntsson/materialdesigns/MainActivity.kt
@@ -2,11 +2,15 @@ package com.jpberntsson.materialdesigns
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.RecyclerView
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        val recyclerView = findViewById<RecyclerView>(R.id.recyclerView)
+        //TODO create adapter
+        //recyclerView.adapter =
     }
 }
