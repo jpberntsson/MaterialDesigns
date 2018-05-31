1828268ebf271559ac3991fe17c7b722e897695f
jpb <john.peter.berntsson@smartrefill.se>
2018-05-31 22:56:58 +0200

    Clean up

--- app/src/main/java/com/jpberntsson/materialdesigns/DesignAdapter.kt
+++ app/src/main/java/com/jpberntsson/materialdesigns/DesignAdapter.kt
@@ -1,9 +1,6 @@
 package com.jpberntsson.materialdesigns
 
 import android.content.Context
-import android.support.v7.widget.RecyclerView
-import android.view.View
-import android.view.ViewGroup
 import android.widget.ArrayAdapter
 
 
